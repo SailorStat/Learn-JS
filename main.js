ac183ec80e1399c1da0ff2be0f678bf8a7fa2dc2
SailorStat <77109932+SailorStat@users.noreply.github.com>
2021-03-05 17:04:01 +0300

    start learn поиск элементов

---
+++ main.js
@@ -0,0 +1,6 @@
+//? Поиск: getElement*, querySelector*
+//* Получение элемента
+// Если у элемента в html есть id, то его получить через document.getElementById('id')
+let element = document.getElementById('main')
+element.style.background = 'rgba(200, 100, 150, .4)'
+
