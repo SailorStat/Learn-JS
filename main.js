bbd69b050ab74b456f7225f9cceb20ce413c046e
SailorStat <77109932+SailorStat@users.noreply.github.com>
2021-03-20 17:03:01 +0300

    start learn move mouse

---
+++ main.js
@@ -0,0 +1,6 @@
+//? Движения мыши: mouseover/out, mouseenter/leave
+//* mouseover, mouseout и relatedTarget
+// Когда курсор перемещается с элемента на элемент,
+// то у 1-го элемента генерируется mouseout
+// у 2-го элемента генеркруется mouseover
+//* relatedTarget - элемент который предшествовал/последует для перехода
