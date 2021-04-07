ce21d958323e2238721fbb85362e84509a27e0b7
SailorStat <77109932+SailorStat@users.noreply.github.com>
2021-04-07 20:48:24 +0300

    start learn formdata

---
+++ main.js
@@ -0,0 +1,48 @@
+//? FormData
+//* const formData = new FormData([form])
+// Если в коструктор передать элемент HTML-формы, то создаваемый объект автоматически прочитает из неё поля
+// в body у fetch позволяет указать FormData
+// Он будет закодирован и отправлен с заголовком Content-Type: form/multipart
+// Для сервера это обычная отправка формы
+{
+document.body.innerHTML = '<form id="formElem"> <input type="text" name="name" value="John"> <input type="text" name="surname" value="Smith"> <input type="submit"></form>'
+formElem.onsubmit = async (e) => {
+  e.preventDefault()
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: new FormData(formElem)
+  })
+  let result = await response.json();
+
+  console.log(result);
+}
+}
+
+//* Методы FormData
+// formData.append(name, value) - добавляет к объекту поле с имененм name и значением value
+// formData.append(name, blob, filename) - добавляет к объекту поле name с файлом blob
+//    и названием файлв filename, как если бы файл был так назван на устройстве
+// formData.delete(name) - удаляет поле name
+// formData.get(name) - получает занчение поля name
+// formData.has(name) - наличие поля name
+
+//TODO технически в форму можно добавить много полей с одинаковым name
+// метод set вместо append гарантирует, что все одинаковые name будут удалены перед добавлением нового
+{
+const formData = new FormData()
+formData.append('name', '1')
+formData.append('name', '1')
+formData.append('name', '1')
+for ([key, value] of formData) {
+  console.log(key, value)
+}
+formData.set('name', '2')
+for ([key, value] of formData) {
+  console.log(key, value)
+}
+}
+
+
+//* Отправка формы файлом
+// FormData отсылаются с заголовком Content-Type: form/multipart
+//    это позволяет отправлять файлы. Даже прописывать не надо.
